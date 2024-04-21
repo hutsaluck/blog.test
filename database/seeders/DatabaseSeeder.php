f3ae518e127473710d4587add7c134cbb57e8c45
Юра Гуцалюк <hutsaluck@gmail.com>
2024-04-21 18:19:33 +0300

    Create blog

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Category::factory(3)->create();
+        Post::factory(10)->create();
+        Comment::factory(20)->create();
         // User::factory(10)->create();
 
         User::factory()->create([
